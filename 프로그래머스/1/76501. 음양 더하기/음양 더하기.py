85dbb667c6d25eeea9adc108e296c457c68e3c79
hueonju <fhseh14@gmail.com>
2026-07-19 15:45:28 +0900

    [level 1] Title: 음양 더하기, Time: 0.10 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,11 @@
+def solution(absolutes, signs):
+    answer = 0
+
+    for i in range (len(absolutes)):
+        sign = signs[i]
+        if signs[i] == False:
+            sign = -1
+        answer += absolutes[i] * sign
+
+
+    return answer
