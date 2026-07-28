4c608afe1a81d3c2f1c2a9a2e4d2ecb56f189bb0
hueonju <fhseh14@gmail.com>
2026-07-28 09:00:47 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -0,0 +1,11 @@
+def solution(phone_number):
+    answer = ''
+
+    phone_number = list(phone_number)
+
+    for i in range(len(phone_number)-4):
+        # phone_number[:-4] = "*"
+        phone_number[i] = "*"
+
+    answer = ''.join(phone_number)
+    return answer
